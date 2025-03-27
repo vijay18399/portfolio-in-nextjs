5eecf1405fe7dfc701a56d4adfeb22b9049058da
vijay18399 <vijayreddy18399@gmail.com>
2025-03-28 00:01:51 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  transpilePackages: ['mui-color-input'],
+  output: "export",
+  basePath: "/portfolio-in-nextjs", 
+  assetPrefix: "/portfolio-in-nextjs/",
   images: {
     remotePatterns: [
       {
